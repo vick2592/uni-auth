6cd97fa9dc3c1cd65bc965b97b097338a6e2d9fd
vick2592 <vick2592@yahoo.com>
2023-09-22 23:58:58 -0400

    GraphQL integration

--- uni-auth/src/App.js
+++ uni-auth/src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Airstack from './components/Airstack.js'; // Corrected import
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Rest of your code */}
+      <Airstack />
+      {/* <p>Test file</p> */}
     </div>
   );
 }
---
+++ uni-auth/src/components/Airstack.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { init, useQuery } from "@airstack/airstack-react";
+init("a611deff724c4969b67a4b819d1f9fa4");
+
+const query = `
+  query MyQuery($identity: Identity) {
+    Socials(
+      input: {
+        filter: {
+          identity: { _eq: $identity }
+          dappSlug: { _eq: lens_polygon }
+        }
+        blockchain: ethereum
+      }
+    ) {
+      Social {
+        dappName
+        dappSlug
+        profileName
+      }
+    }
+  }
+`;
+
+const variables = {
+  identity: "0xcBFBcbFcA74955B8AB75Dec41F7b9eF36F329879",
+};
+
+const Airstack = () => {
+  // Your Airstack component code here
+
+  const { data, loading, error } = useQuery(query, variables, { cache: false });
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  // Render your component using the data returned by the query
+};
+
+export default Airstack;
